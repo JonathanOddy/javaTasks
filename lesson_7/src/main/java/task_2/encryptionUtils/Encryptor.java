f6909fafbb1aad879fb15f42e0637d2091480cb9
”Kirill <aojona@ya.ru>
2022-09-09 18:35:37 +0300

    update lesson_7

--- lesson_7/src/main/java/task_2/encryptionUtils/Encryptor.java
+++ lesson_7/src/main/java/task_2/encryptionUtils/Encryptor.java
@@ -1,23 +1,22 @@
 package task_2.encryptionUtils;
 
 import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
 import java.security.NoSuchAlgorithmException;
 
 public class Encryptor {
 
     private final String ALGORITHM;
-    private SecretKeySpec secretKey;
+    private SecretKey secretKey;
 
-    public Encryptor(String ALGORITHM) {
+    public Encryptor(String ALGORITHM){
         this.ALGORITHM = ALGORITHM;
-        //            KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
-//            keyGenerator.init(keyBitSize);
-//            this.secretKey = keyGenerator.generateKey();
-
-        byte[] keys = {-77, 50, -115, 63, 109, -13, 126, -25, 101, 34, -19, 56, 61, -11, -75, 103};
-        this.secretKey  = new SecretKeySpec(keys, 0, keys.length, "AES");
-
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+            keyGenerator.init(256);
+            this.secretKey = keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
     public byte[] cryptByteArray(byte[] data, int cipherMode) {
@@ -25,7 +24,6 @@ public class Encryptor {
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(cipherMode, this.secretKey);
             return cipher.doFinal(data);
-            // must add padding before doFinal because block size is 16 bytes
         } catch (Exception e) {
             e.printStackTrace();
             return null;
